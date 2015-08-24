ccf72be242c76e298b0cd343a3533521b925ae14
mplacona <marcos.placona@gmail.com>
2015-08-24 13:56:39 +0100

    Initial add

---
+++ TwilioMVCSample/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Twilio;
+using Twilio.TwiML.Mvc;
+
+namespace TwilioMVCSample.Controllers
+{
+    public class HomeController : TwilioController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string AccountSid = "mySID";
+            string AuthToken = "myAuth";
+
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+            return Content(twilio.ApiVersion);
+        }
+    }
+}
---
+++ TwilioMVCSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwilioMVCSample.MvcApplication" Language="C#" %>
